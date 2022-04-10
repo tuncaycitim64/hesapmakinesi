093ee9eb5281c70aa83afc41f1bad7648d7b9ce5
tuncaycitim64 <terkialp18@posta.pau.edu.tr>
2022-04-10 23:23:36 +0300

    ikinci sürümü yükledim

--- ikinci surum/src/hesapmakinesi.java
+++ ikinci surum/src/hesapmakinesi.java
@@ -2,5 +2,9 @@ public class hesapmakinesi {
     public static double toplama(double a,double b) {
         double toplam=a+b;
         return toplam;
-} 
+}
+    public static double bölme(double a,double b) {
+        double bolum=a/b;
+        return bolum;
+    }
 }
